52b80e4b59c56f52cc9ef76ead0107b8a49b142b
Leonid Bagrin <bgrln347@gmail.com>
2022-04-20 17:11:57 +0300

    1.9. Объекты и классы  - ДЗ

---
+++ src/Lesson10/Author.java
@@ -0,0 +1,20 @@
+package Lesson10;
+
+public class Author {
+
+    private String name;
+    private String secondName;
+
+    Author(String name, String secondName) {
+        this.name = name;
+        this.secondName = secondName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+}
